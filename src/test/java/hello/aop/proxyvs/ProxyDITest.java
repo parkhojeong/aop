427575340bb59faf545d90c05336b5f9057c3cf2
hojeong <parkhj062@gmail.com>
2026-01-22 21:23:17 +0900

    [121. 프록시 기술과 한계 - 의존관계 주입]

---
+++ src/test/java/hello/aop/proxyvs/ProxyDITest.java
@@ -0,0 +1,29 @@
+package hello.aop.proxyvs;
+
+import hello.aop.member.MemberService;
+import hello.aop.member.MemberServiceImpl;
+import hello.aop.proxyvs.code.ProxyDIAspect;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+//@SpringBootTest(properties = "spring.aop.proxy-target-class=false") // jdk dynamic proxy
+@SpringBootTest(properties = "spring.aop.proxy-target-class=true") // CGLIB
+@Slf4j
+@Import(ProxyDIAspect.class)
+public class ProxyDITest {
+    @Autowired
+    MemberService memberService;
+
+    @Autowired
+    MemberServiceImpl memberServiceImpl;
+
+    @Test
+    void go() {
+        log.info("memberService class = {}", memberService.getClass());
+        log.info("memberServiceImpl class = {}", memberServiceImpl.getClass());
+        memberService.hello("ok");
+    }
+}
